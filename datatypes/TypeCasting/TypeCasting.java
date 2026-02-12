f98d8030e1d6a69832b8a3bc8f01488c1cdb7878
faqihmujaddid <150465303+faqihmujaddid@users.noreply.github.com>
2026-02-12 22:47:40 +0700

    Add files via upload

---
+++ datatypes/TypeCasting/TypeCasting.java
@@ -0,0 +1,18 @@
+package pkg1.datatypes;
+
+public class TypeCasting {
+
+    static short methodOne(long l) {
+        int i = (int) l;
+        return (short) i;
+    }
+
+    public static void main(String[] args) {
+        double d = 10.25;
+        float f = (float) d;
+        byte b = (byte) methodOne((long) f);
+        System.out.println(b);
+    }
+}
+
+
